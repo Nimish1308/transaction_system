1c4d46e48b8ca329d6ebf423db7198e9252cfdc6
Nimish Mangesh  Shetye <110887620+Nimish1308@users.noreply.github.com>
2025-01-20 11:20:51 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,30 @@
-//Require Modules
+// Require Modules
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const axios=require('axios');
+const axios = require('axios');
 const PORT = 5000;
 const app = express();
 
+// Middleware setup
 app.use(express.json());
-app.use(cors(
-    {
-   origin: 'https://emp-sys-frontend.vercel.app',
-    credentials:true
-}
-))
+app.use(cors({
+    origin: 'https://emp-sys-frontend.vercel.app', // Frontend URL for CORS
+    credentials: true
+}));
 
-
-//API
+// API route to fetch products
 app.get('/backend/products', async (req, res) => {
     try {
         const response = await axios.get('https://s3.amazonaws.com/roxiler.com/product_transaction.json');
-        res.json(response.data);
-        // res.setHeader("Access-Control-Allow-Origin", "*");
+        res.json(response.data); // Send the data to the client
     } catch (error) {
+        console.error('Error fetching data from S3:', error.message);
         res.status(500).json({ error: 'Failed to fetch data from Server' });
     }
 });
 
+// Start server
 app.listen(PORT, () => {
-    console.log(`Server is running at port no:${PORT}`)
-})
+    console.log(`Server is running at port no: ${PORT}`);
+});
